0d90e6fd44e2e67a7a3951d445b06d3180516f26
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-07-18 14:11:51 +0200

    added test for buildInitialBoard

--- test/RenderStateSpec.hs
+++ test/RenderStateSpec.hs
@@ -4,7 +4,7 @@ module RenderStateSpec
 where
 
 import SpecHelper
-import RenderState (emptyBoard, BoardInfo (BoardInfo))
+import RenderState (emptyBoard, BoardInfo (BoardInfo), buildInitialBoard)
 import Data.Array (Array, assocs)
 
 
@@ -14,6 +14,10 @@ spec =
         context "empty grid" $
             it "should be only floors" $
                 (assocs $ emptyBoard (BoardInfo 2 2)) `shouldMatchList` [((1,1) :: Point, Floor), ((1,2) :: Point, Floor), ((2,1) :: Point, Floor), ((2,2) :: Point, Floor)] 
+        context "initialize board" $
+            it "should have player at (1,2)" $ 
+                (assocs $ board (buildInitialBoard (BoardInfo 2 2) (1,2))) `shouldMatchList` [((1,1) :: Point, Floor), ((1,2) :: Point, Player), ((2,1) :: Point, Floor), ((2,2) :: Point, Floor)] 
+
 
 main :: IO ()
 main = hspec spec
