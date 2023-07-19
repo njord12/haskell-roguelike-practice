ebb74164cb80c42d6d6f30ddf0087ad1b892aa22
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-07-19 17:09:43 +0200

    some basic gamestate stuffs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
-import Lib
+import RenderState (buildInitialBoard, render, BoardInfo (BoardInfo))
 
 main :: IO ()
-main = someFunc
+main = do
+    let board = buildInitialBoard (BoardInfo 10 10) (3,3)
+    putStr $ render (BoardInfo 10 10) board
--- src/GameState.hs
+++ src/GameState.hs
@@ -1,2 +1,30 @@
 module GameState
 where
+import RenderState (Point, RenderMessage, BoardInfo (..))
+
+
+data Movement = North | South | East | West | NorthEast | NorthWest | SouthEast | SouthWest
+
+data Player = Player {postition :: Point, hitPoints :: Int} deriving (Show, Eq)
+
+data MapData = MapData {level :: Int, stairsPosition :: Point}
+
+data GameState = GameState {player :: Player, mapData :: MapData, movement :: Movement}
+
+
+-- move :: BoardInfo -> GameState -> (RenderState.RenderMessage, GameState)
+-- move bi (GameState player map mov) = 
+--     let
+
+
+moveHelper :: Movement -> Player -> BoardInfo -> Point
+moveHelper mov (Player (y,x) _) (BoardInfo h w) =
+    case mov of
+        North -> (y - 1, x)
+        South -> (y + 1, x)
+        East -> (y, x + 1)
+        West -> (y, x - 1)
+        NorthEast -> (y - 1, x + 1)
+        NorthWest -> (y - 1, x - 1)
+        SouthEast -> (y + 1, x + 1)
+        SouthWest -> (y + 1, x - 1)
