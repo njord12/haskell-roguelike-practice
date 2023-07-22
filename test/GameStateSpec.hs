fc9638c9007c051be00d8e1cfea716f13fbfbd99
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-07-22 09:37:33 +0200

    refactor gamestate spec to make it a bit more readable

--- test/GameStateSpec.hs
+++ test/GameStateSpec.hs
@@ -4,20 +4,23 @@ where
 import SpecHelper
 import GameState (moveHelper)
 import Types 
-import RenderState (BoardInfo(BoardInfo))
 
 
 spec :: Spec
 spec = 
     describe "GameState tests" $ do
         it "moving north on the upper edge should not change position" $
-            moveHelper North (PlayerData (1,1) 0) (BoardInfo 2 2) `shouldBe` (1,1)
+            moveHelper North (PlayerData (1,1) 0) getDefaultBoardInfo `shouldBe` (1,1)
 
         it "moving south on the lower edge should not change position" $
-            moveHelper South (PlayerData (2,1) 0) (BoardInfo 2 2) `shouldBe` (2,1)
+            moveHelper South (PlayerData (2,1) 0) getDefaultBoardInfo `shouldBe` (2,1)
 
         it "non-edge movement should change position" $
-            moveHelper East (PlayerData (1,1) 0) (BoardInfo 2 2) `shouldBe` (1,2)
+            moveHelper East (PlayerData (1,1) 0) getDefaultBoardInfo `shouldBe` (1,2)
 
         it "diagonal movement should work while not moving past corner" $
-            moveHelper SouthEast (PlayerData (1,1) 0) (BoardInfo 2 2) `shouldBe` (2,2)
+            moveHelper SouthEast (PlayerData (1,1) 0) getDefaultBoardInfo `shouldBe` (2,2)
+
+
+getDefaultBoardInfo :: BoardInfo
+getDefaultBoardInfo = BoardInfo 2 2
