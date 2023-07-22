feda7945108c9158008ab9594bafb82716e6c545
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-07-22 09:34:06 +0200

    added movement tests in GameStateSpec

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,10 +1,14 @@
 module SpecHelper
     (module Test.Hspec,
     module Test.Hspec.Runner,
-    module RenderState
+    module RenderState,
+    module GameState,
+    module Types
     )
 where
 
 import Test.Hspec
 import Test.Hspec.Runner
 import RenderState
+import GameState
+import Types
