6266985ec2cd3ac1cc4c43bc5e99d8ec117a061c
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-07-27 14:03:01 +0200

    slowly adding test cases

--- test/GameStateSpec.hs
+++ test/GameStateSpec.hs
@@ -4,12 +4,16 @@ where
 import SpecHelper
 import Entities (makePlayer)
 import Data.Array (assocs)
+import Types (Movement(NorthEast))
+import GameState (initializeMap)
 
 spec :: Spec
 spec =
     describe "GameState tests" $ do
         it "moving north on the upper edge should not change position" $
            getAssocs (move getDefaultBoardInfo North makePlayer (initializeMap getDefaultBoardInfo)) `shouldMatchList` getAssocs (initializeMap getDefaultBoardInfo)
+        it "moving diagonally towards a border should not change position" $
+            getAssocs (move getDefaultBoardInfo NorthEast makePlayer (initializeMap getDefaultBoardInfo)) `shouldMatchList` getAssocs (initializeMap getDefaultBoardInfo)
 
 
 getDefaultBoardInfo :: GridSize
