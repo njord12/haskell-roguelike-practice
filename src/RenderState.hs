049de1d315f8ccced5f1bf357fbfcd50e21ff039
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-08-10 10:31:45 +0200

    fix indentation

--- src/RenderState.hs
+++ src/RenderState.hs
@@ -1,28 +1,25 @@
 {-# LANGUAGE BangPatterns #-}
-module RenderState (updateRenderState, emptyBoard, render)
-where
 
+module RenderState (updateRenderState, emptyBoard, render) where
 
 import Data.Array (listArray, (//))
 import Data.Foldable (foldl')
-import Types (RenderState(..), GridSize (..), Board, RenderMessage(..), RenderState, TileType (..), Grid, Cell (Cell), entityGlyph, Tile (tileGlyph))
-
+import Types (Board, Cell (Cell), Grid, GridSize (..), RenderMessage (..), RenderState (..), Tile (tileGlyph), TileType (..), entityGlyph)
 
 emptyBoard :: GridSize -> Board
 emptyBoard (BoardInfo height width) =
-    listArray bounds emptyCells
-    where
-        bounds = ((1,1), (height, width))
-        emptyCells = replicate (height * width) Floor
-
+  listArray bounds emptyCells
+  where
+    bounds = ((1, 1), (height, width))
+    emptyCells = replicate (height * width) Floor
 
 updateRenderState :: RenderState -> RenderMessage -> RenderState
 updateRenderState (RenderState b gO) message =
-    case message of
-        RenderBoard delta ->
-            RenderState (b // delta) gO
-        GameOver ->
-            RenderState b True
+  case message of
+    RenderBoard delta ->
+      RenderState (b // delta) gO
+    GameOver ->
+      RenderState b True
 
 render :: GridSize -> Grid -> String
 render (BoardInfo _ w) g =
@@ -30,9 +27,9 @@ render (BoardInfo _ w) g =
   where
     gridToString = foldl' fprint ("", 0)
     fprint (!s, !i) cell =
-      if((i + 1) `mod` w) == 0
+      if ((i + 1) `mod` w) == 0
         then (s <> decideGlyph cell <> "\n", i + 1)
-        else (s <> decideGlyph cell, i+ 1)
+        else (s <> decideGlyph cell, i + 1)
     decideGlyph (Cell e t) =
       case e of
         Just entity -> entityGlyph entity : " "
