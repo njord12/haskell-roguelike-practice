91fe228028c260e212344391fab36498a38f07f5
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-08-10 14:23:38 +0200

    can make rooms into (point,cell) lists

--- src/DungeonGenerator.hs
+++ src/DungeonGenerator.hs
@@ -1,21 +1,40 @@
 module DungeonGenerator
 where
-import Types (Point)
+import Types (Point, Cell(..), GridSize(..))
 import RandomGenerator
 import System.Random
+import Entities (wallTile, floorTile)
 
-data RectangularRoom = RectangularRoom {corner :: Point,corner2 :: Point, height :: Int, width :: Int}
+data RectangularRoom = RectangularRoom {corner :: Point,corner2 :: Point, height :: Int, width :: Int} deriving Show
 
 mkRectangularRoom :: Point -> Int -> Int -> RectangularRoom
-mkRectangularRoom (y, x) h w = RectangularRoom (y, x) (y + h,x + w) h w
+mkRectangularRoom (y, x) h w = RectangularRoom (y, x) (y + h - 1,x + w - 1) h w
 
 center :: RectangularRoom -> Point
 center (RectangularRoom (y1,x1) (y2,x2) _ _) = ((y1 + y2) `div` 2, (x1 + x2) `div` 2)
 
-innerRoom :: RectangularRoom -> ([Int], [Int])
+innerRoom :: RectangularRoom -> [Point]
 innerRoom (RectangularRoom (y1, x1) (y2,x2) _ _) =
     let
-        ySlice = [y1 + 1 .. y2]
-        xSlice = [x1 + 1 .. x2]
+        ySlice = [y1 + 1 .. y2 - 1]
+        xSlice = [x1 + 1 .. x2 - 1]
     in
-        (ySlice, xSlice)
+        [(y,x) | y <- ySlice, x <- xSlice]
+
+roomToCellList :: RectangularRoom -> [(Point, Cell)]
+roomToCellList room@(RectangularRoom (y1, x1) (y2, x2) _ _) =
+    let
+        edge = [(y,x) | y <- [y1 .. y2], x <- [x1 .. x2]]
+        inside = innerRoom room
+        walls = [(p, Cell Nothing wallTile) | p <- edge ]
+        floors = [(p, Cell Nothing floorTile) | p <- inside]
+    in
+        walls <> floors
+
+makeDungeon :: GridSize -> [(Point, Cell)]
+makeDungeon (GridSize h w) =
+    let
+        room1 = mkRectangularRoom (2,2) 4 4
+        room2 = mkRectangularRoom (2,8) 3 3
+    in
+        roomToCellList room1 <> roomToCellList room2
