397bbce8385d95ab4b89891a95760ad1a51ae50c
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-08-12 10:18:39 +0200

    initial implementation of bresenhams line algorithm

--- src/DungeonGenerator.hs
+++ src/DungeonGenerator.hs
@@ -4,6 +4,7 @@ import Types (Point, Cell(..), GridSize(..))
 import RandomGenerator
 import System.Random
 import Entities (wallTile, floorTile)
+import Data.List (unfoldr, sort)
 
 data RectangularRoom = RectangularRoom {corner :: Point,corner2 :: Point, height :: Int, width :: Int} deriving Show
 
@@ -34,7 +35,32 @@ roomToCellList room@(RectangularRoom (y1, x1) (y2, x2) _ _) =
 makeDungeon :: GridSize -> [(Point, Cell)]
 makeDungeon (GridSize h w) =
     let
-        room1 = mkRectangularRoom (2,2) 4 4
-        room2 = mkRectangularRoom (2,8) 3 3
+        room1 = mkRectangularRoom (2,2) 6 6
+        room2 = mkRectangularRoom (11,2) 4 5
+        path = [(p, Cell Nothing floorTile) | p <- line (center room1) (center room2)] 
     in
-        roomToCellList room1 <> roomToCellList room2
+        roomToCellList room1 <> roomToCellList room2 <> path
+
+line :: Point -> Point -> [Point]
+line pa@(xa,ya) pb@(xb,yb) = map maySwitch . unfoldr go $ (x1,y1,0)
+  where
+    steep = abs (yb - ya) > abs (xb - xa)
+    maySwitch = if steep then (\(x,y) -> (y,x)) else id
+    [(x1,y1),(x2,y2)] = sort [maySwitch pa, maySwitch pb]
+    deltax = x2 - x1
+    deltay = abs (y2 - y1)
+    ystep = if y1 < y2 then 1 else -1
+    go (xTemp, yTemp, error)
+        | xTemp > x2 = Nothing
+        | otherwise  = Just ((xTemp, yTemp), (xTemp + 1, newY, newError))
+        where
+          tempError = error + deltay
+          (newY, newError) = if (2*tempError) >= deltax
+                            then (yTemp+ystep,tempError-deltax)
+                            else (yTemp,tempError)
+
+bresenhamLine :: Point -> Point -> [Point]
+bresenhamLine p1 p2 = map swap (line p1 p2)
+
+swap :: Point -> Point
+swap (x,y) = (y,x)
